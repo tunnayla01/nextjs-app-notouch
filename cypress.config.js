c454ac6691696dabf576a1cf97c92bdce60c8881
tunnayla <tunnayla01.edu@gmail.com>
2025-10-16 01:56:14 +0700

    finaldakalpar

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
+// cypress.config.js
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000", // Change to your dev URL
+    baseUrl: "http://3.1.146.103", // Your Lightsail server
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    supportFile: "cypress/support/e2e.js"
+    supportFile: "cypress/support/e2e.js" // even an empty file is fine
   }
 });
 
